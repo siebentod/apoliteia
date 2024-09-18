d225410f9778cf52613459f86f1c4435481e0af8
siebentod <33163165+siebentod@users.noreply.github.com>
2024-09-18 14:15:51 +0300

    smallfix

--- app/_components/PageStructure/Column3.jsx
+++ app/_components/PageStructure/Column3.jsx
@@ -2,21 +2,27 @@
 
 import Translation from './Translation';
 import { usePageStore } from '../store';
+import { useEffect, useState } from 'react';
 
 function Column3({ id, translations, textObject, children }) {
   const transChanged = usePageStore((state) => state.transChanged);
+  const [trans, useTrans] = useState(textObject[0]);
 
-  return (
-    <>
-      {transChanged !== false ? (
+  useEffect(() => {
+    if (transChanged !== false) {
+      useTrans(
         <Translation
           id={id}
           translations={translations}
           textObject={textObject}
         />
-      ) : (
-        children
-      )}
+      );
+    }
+  }, [transChanged]);
+
+  return (
+    <>
+      {trans}
       <p key="column3" className="endOfColumn endOfColumn2"></p>
     </>
   );
