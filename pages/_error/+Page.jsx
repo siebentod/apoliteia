db8dd3f314f3a6fb0549fcab7199b4c1225eb3c2
siebentod <33163165+siebentod@users.noreply.github.com>
2024-09-19 16:41:19 +0300

    404 try

--- pages/_error/+Page.jsx
+++ pages/_error/+Page.jsx
@@ -1,33 +1,35 @@
+import { Helmet } from 'react-helmet';
+import './error-page.scss';
+
 export { Page };
 
-import { usePageContext } from '../../renderer/usePageContext';
+// import { usePageContext } from '../../renderer/usePageContext';
 
 function Page() {
-  const pageContext = usePageContext();
-  let { abortReason } = pageContext;
-  if (!abortReason) {
-    abortReason = pageContext.is404
-      ? 'Page not found.'
-      : 'Something went wrong.';
-  }
+  // const pageContext = usePageContext();
+  // let { abortReason } = pageContext;
+  // if (!abortReason) {
+  //   abortReason = pageContext.is404
+  //     ? 'Page not found.'
+  //     : 'Something went wrong.';
+  // }
   return (
-    <Center>
-      <p style={{ fontSize: '1.3em' }}>{abortReason}</p>
-    </Center>
-  );
-}
-
-function Center({ children }) {
-  return (
-    <div
-      style={{
-        height: 'calc(100vh - 100px)',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      {children}
+    <div id="error-page">
+      <Helmet>
+        <title>404</title>
+      </Helmet>
+      <div className="error-page__header">
+        <h1 style={{ textAlign: 'center' }}>404 </h1>
+        <p>Страница не найдена</p>
+      </div>
+      <div
+        className="error-page__main"
+        style={{ textAlign: 'center', color: 'grey' }}
+      >
+        <p>
+          Go to <a href="/">Homepage</a>
+        </p>
+      </div>
     </div>
   );
 }
